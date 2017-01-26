bbf017e389df41b1ecd883d15a12aa8b8becf775
github username <github email address>
2017-01-26 13:07:35 -0500

    c to f converter

---
+++ temp_converter.py
@@ -0,0 +1,6 @@
+def convert():
+    celsius = int(raw_input("Enter degrees in C: "))
+    fahrenheit = 9/5 * celsius + 32
+    print "The temperature is", fahrenheit, "degrees in fahrenheit."
+
+convert()
